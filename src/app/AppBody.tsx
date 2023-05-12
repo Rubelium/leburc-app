10bd9fb11b5e8eab8a442a4113f52d342e51a00f
Rubelium <105171977+Rubelium@users.noreply.github.com>
2023-05-12 16:04:28 -0400

    Update theme coloring

--- src/app/AppBody.tsx
+++ src/app/AppBody.tsx
@@ -3,10 +3,10 @@ import { SocialIcon } from "react-social-icons";
 function AppBody() {
   return (
     <div className="App-body">
-      <SocialIcon className="App-social-icon" url="https://soundcloud.com/leburc" />
-      <SocialIcon className="App-social-icon" url="https://www.instagram.com/_leburc/" bgColor="#fa2e4f" />
-      <SocialIcon className="App-social-icon" url="https://github.com/Rubelium/leburc-app" bgColor="#cdd9e5" />
-      <SocialIcon className="App-social-icon" url="mailto:dj.leburc@gmail.com" bgColor="#32a7f4" />
+      <SocialIcon className="App-social-icon" url="https://soundcloud.com/leburc" bgColor="#00ff89" />
+      <SocialIcon className="App-social-icon" url="https://www.instagram.com/_leburc/" bgColor="#00ff89" />
+      <SocialIcon className="App-social-icon" url="https://github.com/Rubelium/leburc-app" bgColor="#00ff89" />
+      <SocialIcon className="App-social-icon" url="mailto:dj.leburc@gmail.com" bgColor="#00ff89" />
     </div>
   );
 }
