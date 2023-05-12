c7687b5f553ee522a786dec3fc7ff13e46a1ea20
Rubelium <105171977+Rubelium@users.noreply.github.com>
2023-05-11 23:59:31 -0400

    add react-spinners-kit

--- src/AppFooter.tsx
+++ src/AppFooter.tsx
@@ -1,9 +1,10 @@
 // import ToViLogo from './logoToVi.png';
+import { ClapSpinner } from "react-spinners-kit";
 
 function AppFooter() {
   return (
     <footer className="App-footer">
-      ...
+      <ClapSpinner size={50} color="#686769" loading={true} />
       {/* <a href="https://www.instagram.com/tovi_ent/">
         <img src={ToViLogo} className="App-tovi-logo" alt="Loading..." />
       </a> */}
