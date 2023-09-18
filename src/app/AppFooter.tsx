932a10e3308107a71cabfa1122befc81a5003a45
Cody Rubel <105171977+Rubelium@users.noreply.github.com>
2023-09-18 15:01:22 -0400

    Update footer tap targets

--- src/app/AppFooter.tsx
+++ src/app/AppFooter.tsx
@@ -1,5 +1,4 @@
-import headshot from "../images/headshot.avif"
-import { Avatar, Link } from "@nextui-org/react";
+import { Link } from "@nextui-org/react";
 import { SocialIcon } from "react-social-icons";
 
 function AppFooter() {
@@ -12,16 +11,16 @@ function AppFooter() {
         <SocialIcon className="App-social-icon mr-[10px] ml-[10px] h-[60px] w-[60px]" url="mailto:dj.leburc@gmail.com" bgColor="#00ff89" />
       </div>
       <div className="flex pt-10 pb-4">
-        <Avatar className="mt-1" isBordered size="md" color="default" src={headshot} />
+        {/* <Avatar className="mt-3" isBordered size="md" color="default" src={headshot} /> */}
         <div className="pl-4">
           <div className="text-xs font-bold">
-            DJ / Website Developer
+            DJ / Aspiring Producer / Developer
           </div>
-          <div className="text-xs">
-            <Link href="mailto:dj.leburc@gmail.com">dj.leburc@gmail.com</Link>
+          <div className="text-xs pt-2">
+            Email: <Link className="pt-0.5 pb-0.5 hover:underline" href="mailto:dj.leburc@gmail.com">dj.leburc@gmail.com</Link>
           </div>
-          <div className="text-xs">
-            <Link href="https://www.instagram.com/leburc.music/">@leburc.music</Link>
+          <div className="text-xs pt-1">
+            Instagram: <Link className="pt-0.5 pb-0.5 hover:underline" href="https://www.instagram.com/leburc.music/">@leburc.music</Link>
           </div>
         </div>
       </div>
