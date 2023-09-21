514f7f9124d00535b9b6a08cdd0ff6e3b8132bbc
Rubelium <105171977+Rubelium@users.noreply.github.com>
2023-09-21 18:28:47 -0400

    Add more playlists

--- src/app/AppPlaylists.tsx
+++ src/app/AppPlaylists.tsx
@@ -1,6 +1,8 @@
+import boomerangsDownUnderBar from "../images/boomerangsDownUnderBar.avif"
 import club201 from "../images/club201.avif"
 import odysseyBeach from "../images/odysseyBeach.avif"
 import stPeteBrewingCompany from "../images/stPeteBrewingCompany.avif"
+import saigonBlonde3 from "../images/saigonBlonde3.avif"
 import saigonBlonde6 from "../images/saigonBlonde6.avif"
 import AppPlaylistCard from "./AppPlaylistCard";
 
@@ -14,7 +16,7 @@ function AppPlaylists() {
           url={"https://soundcloud.com/leburc/sets/minimal-deep-tech-mixes"}
           image={club201}
           imageCreditText="@sookie_takes_photos"
-          imageCreditUrl="https://instagram.com/sookie_takes_photos?igshid=MzRlODBiNWFlZA==" />
+          imageCreditUrl="https://instagram.com/sookie_takes_photos" />
         <div className="pt-1">
           <AppPlaylistCard
             headerText={"MIX PLAYLIST"}
@@ -22,7 +24,7 @@ function AppPlaylists() {
             url={"https://soundcloud.com/leburc/sets/afro-house-organic-house"}
             image={saigonBlonde6}
             imageCreditText="@sookie_takes_photos"
-            imageCreditUrl="https://instagram.com/sookie_takes_photos?igshid=MzRlODBiNWFlZA==" />
+            imageCreditUrl="https://instagram.com/sookie_takes_photos" />
         </div>
       </div>
       <div>
@@ -36,11 +38,31 @@ function AppPlaylists() {
         <div className="pt-1">
           <AppPlaylistCard
             headerText={"MIX PLAYLIST"}
-            subHeaderText={"Miscellaneous Series"}
-            url={"https://soundcloud.com/leburc/sets/other-mixes"}
+            subHeaderText={"Indie Dance Series"}
+            url={"https://soundcloud.com/leburc/sets/mixes-indie-dance"}
             image={stPeteBrewingCompany}
             imageCreditText="@f2t4"
             imageCreditUrl="https://www.instagram.com/f2t4/" />
+        </div>
+      </div>
+      <div>
+        <AppPlaylistCard
+          headerText={"MIX PLAYLIST"}
+          subHeaderText={"Deep House / Progressive House Series"}
+          url={"https://soundcloud.com/leburc/sets/mixes-deep-house-progressive"}
+          image={saigonBlonde3}
+          imageCreditText="@sookie_takes_photos"
+          imageCreditUrl="https://instagram.com/sookie_takes_photos"
+        />
+        <div className="pt-1">
+          <AppPlaylistCard
+            headerText={"MIX PLAYLIST"}
+            subHeaderText={"Miscellaneous Series"}
+            url={"https://soundcloud.com/leburc/sets/other-mixes"}
+            image={boomerangsDownUnderBar}
+            imageCreditText="@sandorimusic"
+            imageCreditUrl="https://www.instagram.com/sandorimusic/"
+          />
         </div>
       </div>
     </>
