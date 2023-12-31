b78b3edf7362281d074e8fb25587b3ad14bfc0d1
Rubelium <105171977+Rubelium@users.noreply.github.com>
2023-12-30 23:44:50 -0500

    Undo: Update latest mix card to neon purple

--- src/app/AppLatestMixCard.tsx
+++ src/app/AppLatestMixCard.tsx
@@ -4,7 +4,7 @@ import { Card, CardHeader, CardBody } from "@nextui-org/react";
 function AppLatestMixCard() {
   return (
     <Card
-      className="py-2 m-9 mt-0 border-2 max-w-[688px] border-[#B026FF] rounded-xl bg-[#ffffff] text-black shadow-lg shadow-[#B026FF]"
+      className="py-2 m-9 mt-0 border-2 max-w-[688px] border-[#fa5300] rounded-xl bg-[#ffffff] text-black shadow-lg shadow-[#fa5300]"
       fullWidth
     >
       <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
