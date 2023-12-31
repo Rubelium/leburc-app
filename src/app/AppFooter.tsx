6b895ceef94b6234a40a474f800350ec1447a3e9
Rubelium <105171977+Rubelium@users.noreply.github.com>
2023-12-31 14:37:11 -0500

    Update last updated date

--- src/app/AppFooter.tsx
+++ src/app/AppFooter.tsx
@@ -47,7 +47,7 @@ function AppFooter() {
           </div>
         </div>
       </div>
-      <div className="text-xs text-white font-thin pt-6">&copy; LEBURC 2023 • Last updated: 12/27/2023</div>
+      <div className="text-xs text-white font-thin pt-6">&copy; LEBURC 2023 • Last updated: 12/31/2023</div>
     </footer>
   );
 }
