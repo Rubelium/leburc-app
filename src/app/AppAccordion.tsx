21fa5253edd407817369fb100e7696fbb90ce93f
Rubelium <105171977+Rubelium@users.noreply.github.com>
2024-03-12 09:30:52 -0400

    Update bio text

--- src/app/AppAccordion.tsx
+++ src/app/AppAccordion.tsx
@@ -39,7 +39,7 @@ function AppAccordion() {
           >
             <p className="pr-1">Under Construction . . .</p>
           </Chip>
-          <p className="pt-5 text-sm font-thin">Currently working meticulously on my hopeful first release as a producer. Creating minimal deep tech compositions.</p>
+          <p className="pt-5 text-sm font-thin">Currently working on my hopeful first release as a producer. Creating minimal deep tech compositions.</p>
         </div>
       </AccordionItem>
     </Accordion>
